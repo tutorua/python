542cc8140f1d825de312f6acda5eab1abe6e5067
Igor Goncharov <tutorua@ukr.net>
2021-09-29 13:14:39 +0300

    uploaded tasks 1, 2, 3

---
+++ shwalenghthimeter.py
@@ -0,0 +1,22 @@
+# Task #3
+import re
+
+# Data
+data = ['apple', 'banana', 'tractor', 'garik', 'balkon']
+vowels = set('aeoiuy')
+prefix = 'shwa'
+rule3  = r"()"
+# rule 4 - add space " " to the end
+rule4 = r"()"
+# rule 5 - add lenght of original string to the end
+
+for item in data:
+    new = item[1:]
+    if set(new[0]).issubset(vowels):
+        new1 = prefix + new[1:] 
+    else:
+        new1 = prefix + new
+    result = new1 + ' ' + str(len(item))
+    print(item, result)
+
+print("Done")
