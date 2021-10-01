240ef236ae7682e37682a28b18f2e72d1f013103
Igor Goncharov <tutorua@ukr.net>
2021-10-01 19:23:49 +0300

    Deleted unneeded lines in swalenghthimeter.py

--- shwalenghthimeter.py
+++ shwalenghthimeter.py
@@ -5,15 +5,10 @@ import re
 data = ['apple', 'banana', 'tractor', 'garik', 'balkon']
 vowels = set('aeoiuy')
 prefix = 'shwa'
-rule3  = r"()"
-# rule 4 - add space " " to the end
-rule4 = r"()"
-# rule 5 - add lenght of original string to the end
 
 for item in data:
     new = item[1:]
     if new[0] in vowels:
-    #if set(new[0]).issubset(vowels):
         new1 = prefix + new[1:] 
     else:
         new1 = prefix + new
