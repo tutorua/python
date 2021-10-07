abe6443a0ee81681bd0475e7edcad43fa6e0ed28
Igor Goncharov <tutorua@ukr.net>
2021-10-07 12:47:54 +0300

    Update directory structure

--- reverse.py
+++ reverse.py
@@ -10,9 +10,6 @@ init_string = '1234567890'
 print(init_string) 
 print(list(init_string))
 
-reversed_string = []
-reversed_string.append(list(init_string).pop())
-print(reversed_string)
 
 print(''.join(reversed(init_string))) 
 print(init_string[::-1])
