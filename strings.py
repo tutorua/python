e81fe54d23b6ceb2b7e312c92bf001e381fcc1b8
Igor <tutorua@ukr.net>
2024-07-17 10:56:18 +0300

    Fixed print from v2.7 into v 3.x

--- strings.py
+++ strings.py
@@ -7,19 +7,19 @@ Created on Tue Sep 06 20:25:12 2016
 
 hello = 'hello'   # String literals can use single quotes
 world = "world"   # or double quotes; it does not matter.
-print hello       # Prints "hello"
-print len(hello)  # String length; prints "5"
+print(hello)       # Prints "hello"
+print(len(hello))  # String length; prints "5"
 hw = hello + ' ' + world  # String concatenation
-print hw  # prints "hello world"
+print(hw)  # prints "hello world"
 hw12 = '%s %s %d' % (hello, world, 12)  # sprintf style string formatting
-print hw12  # prints "hello world 12"
+print(hw12)  # prints "hello world 12"
 
 
 s = "hello"
-print s.capitalize()  # Capitalize a string; prints "Hello"
-print s.upper()       # Convert a string to uppercase; prints "HELLO"
-print s.rjust(7)      # Right-justify a string, padding with spaces; prints "  hello"
-print s.center(7)     # Center a string, padding with spaces; prints " hello "
-print s.replace('l', '(ell)')  # Replace all instances of one substring with another;
+print(s.capitalize())  # Capitalize a string; prints "Hello"
+print(s.upper())       # Convert a string to uppercase; prints "HELLO"
+print(s.rjust(7))      # Right-justify a string, padding with spaces; prints "  hello"
+print(s.center(7))     # Center a string, padding with spaces; prints " hello "
+print(s.replace('l', '(ell)'))  # Replace all instances of one substring with another;
                                # prints "he(ell)(ell)o"
-print '  world '.strip()  # Strip leading and trailing whitespace; prints "world"
+print('  world '.strip())  # Strip leading and trailing whitespace; prints "world"
